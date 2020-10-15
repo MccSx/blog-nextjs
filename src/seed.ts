91d310a9f2f63c27994009c1467427e5515b3b35
MccSx <shixiangcool@163.com>
2020-10-15 15:12:11 +0800

    清空数据库

--- src/seed.ts
+++ src/seed.ts
@@ -1,17 +1,9 @@
-import { Post } from "./entity/Post";
+// import { Post } from "./entity/Post";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 
 createConnection()
   .then(async connection => {
-    const posts = await connection.manager.find(Post);
-    if (posts.length === 0) {
-      await connection.manager.save(
-        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(n => {
-          return new Post({ title: `Post_${n}`, content: `我的第${n}篇博客` });
-        })
-      );
-    }
     connection.close();
   })
   .catch(error => console.log(error));
